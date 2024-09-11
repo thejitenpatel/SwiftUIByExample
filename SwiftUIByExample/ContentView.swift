0bce1919ea7d5dd9752bafa7bdc7d7775ea92376
thejitenpatel <jiten.pateljp2001@gmail.com>
2024-09-11 08:22:38 +0530

    Initial Commit

---
+++ SwiftUIByExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIByExample
+//
+//  Created by Jiten Patel on 11/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIByExample/SwiftUIByExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIByExampleApp.swift
+//  SwiftUIByExample
+//
+//  Created by Jiten Patel on 11/09/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIByExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
